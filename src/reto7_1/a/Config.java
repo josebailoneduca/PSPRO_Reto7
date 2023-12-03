91d79acd090ef504ee135217c1217b62ff84903e
Jose Javier Bailón Ortiz <josebailon@gmail.com>
2023-12-04 00:41:28 +0100

    Inicio

---
+++ src/reto7_1/a/Config.java
@@ -0,0 +1,14 @@
+package reto7_1.a;
+
+public class Config {
+	public static final int PLAZAS_COMIDA=3;
+	public static final int PLAZAS_COLUMPIO=1;
+	
+	public static final int MIN_COMER=1000;
+	public static final int MAX_COMER=9000;
+	
+	public static final int MIN_COLUMPIARSE=1000;
+	public static final int MAX_COLUMPIARSE=3000;
+	
+	public static final int N_LOROS=8;
+}
---
+++ src/reto7_1/a/Loro.java
@@ -0,0 +1,68 @@
+package reto7_1.a;
+
+import java.util.Random;
+
+public class Loro implements Runnable {
+
+	private int id;
+	
+	public Loro(int id) {
+		this.id = id;
+	}
+
+	@Override
+	public void run() {
+		
+		while(true) {
+			
+			comer();
+			
+			columpiarse();
+			
+		}
+
+	}
+
+	private void columpiarse() {
+		try {
+				
+			long iniT=System.currentTimeMillis();
+			
+			Jaula.columpio.acquire();
+				
+				Estadistica.columpiarse(id, System.currentTimeMillis()-iniT);
+			
+			Thread.currentThread().sleep(new Random().nextLong(Config.MIN_COLUMPIARSE,Config.MAX_COLUMPIARSE));
+			
+			Estadistica.setEstado(id, Estadistica.ESPERA_COMER);
+			Jaula.columpio.release();
+			
+			
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	private void comer() {
+		try {
+			Estadistica.setEstado(id, Estadistica.ESPERA_COMER);
+			
+			long iniT=System.currentTimeMillis();
+			
+			Jaula.comida.acquire();
+			Estadistica.comer(id, System.currentTimeMillis()-iniT);
+
+			Thread.currentThread().sleep(new Random().nextLong(Config.MIN_COMER,Config.MAX_COMER));
+			
+			Estadistica.setEstado(id, Estadistica.ESPERA_COLUMPIO);
+			
+			Jaula.comida.release();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+}
